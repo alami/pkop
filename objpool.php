2595edbb6d84d56b556273bc8f3d6204040dcc1e
alami <alami@km.ru>
2014-03-10 15:58:02 +0500

    pattern registry

---
+++ objpool.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Пул объектов
+ */
+class Factory
+{
+
+    /**
+     * @var Product[]
+     */
+    protected static $products = array();
+
+
+    /**
+     * Добавляет продукт в пул
+     *
+     * @param Product $product
+     * @return void
+     */
+    public static function pushProduct(Product $product)
+    {
+        self::$products[$product->getId()] = $product;
+    }
+
+    /**
+     * Возвращает продукт из пула
+     *
+     * @param integer|string $id - идентификатор продукта
+     * @return Product $product
+     */
+    public static function getProduct($id)
+    {
+        return isset(self::$products[$id]) ? self::$products[$id] : null;
+    }
+
+    /**
+     * Удаляет продукт из пула
+     *
+     * @param integer|string $id - идентификатор продукта
+     * @return void
+     */
+    public static function removeProduct($id)
+    {
+        if (isset(self::$products[$id])) {
+            unset(self::$products[$id]);
+        }
+    }
+}
+
+class Product
+{
+
+    /**
+     * @var integer|string
+     */
+    protected $id;
+
+
+    public function __construct($id) {
+        $this->id = $id;
+    }
+
+    /**
+     * @return integer|string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
+/*
+ * =====================================
+ *         USING OF OBJECT POOL
+ * =====================================
+ */
+
+Factory::pushProduct(new Product('first'));
+Factory::pushProduct(new Product('second'));
+
+print_r(Factory::getProduct('first')->getId());
+// first
+print_r(Factory::getProduct('second')->getId());
+// second
